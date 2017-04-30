273db53165b188be303829eb2a630e810f7c6445
Jonny <petmue@me.com>
2017-04-30 07:40:05 +0200

    Ssl finalization

--- haproxy/startup.sh
+++ haproxy/startup.sh
@@ -1,8 +1,45 @@
+{ \
+echo "global"; \
+echo "    maxconn 100"; \
+echo "    log /sidecar/log local0"; \
+
+echo "defaults"; \
+echo "    log global"; \
+echo "    mode http"; \
+echo "    option httpclose"; \
+echo "    timeout connect 5000ms"; \
+echo "    timeout client 50000ms"; \
+echo "    timeout server 50000ms"; \
+
+echo "frontend http-in"; \
+echo "    bind *:80"; \
+} >> /usr/local/etc/haproxy/haproxy.cfg
 if ${GEN_CERT}; then
     /certbot/certbot-auto certonly --standalone --agree-tos -m ${EMAIL} -n -d ${EXTERNAL_URL}
     cat /etc/letsencrypt/live/${EXTERNAL_URL}/cert.pem privkey.pem > haproxy.pem
+    { \
+    echo "    bind *:443 ssl crt /etc/letsencrypt/live/${EXTERNAL_URL}/haproxy.pem"; \
+    echo "    redirect scheme https if !{ ssl_fc }"; \
+    } >> /usr/local/etc/haproxy/haproxy.cfg
 else
     echo "don't create ssl certificate"
+    { \
+    echo "    acl has_web1 path_beg /web1"; \
+    echo "    acl has_web2 path_beg /web2"; \
+
+    echo "    use_backend web1 if has_web1"; \
+    echo "    use_backend web2 if has_web2"; \
+
+    echo "    default_backend web2"; \
+
+    echo "backend web1"; \
+    echo "    reqrep ^([^\ ]*\ /)web1[/]?(.*)     \1\2"; \
+    echo "    server web1 web1:80 check"; \
+
+    echo "backend web2"; \
+    echo "    reqrep ^([^\ ]*\ /)web2[/]?(.*)     \1\2"; \
+    echo "    server web2 web2:80 check"; \
+    } >> /usr/local/etc/haproxy/haproxy.cfg
 fi
 echo "@monthly root /certbot/certbot-auto certonly --quiet --standalone --renew-by-default -d ${EXTERNAL_URL} >> /var/log/certbot/certbot-auto-update.log" | tee --append /etc/crontab
 haproxy -f /usr/local/etc/haproxy/haproxy.cfg
