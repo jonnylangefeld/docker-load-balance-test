96699b7c247a64784272205743447743d4ce64ae
Jonny <petmue@me.com>
2017-04-30 07:02:15 +0200

    fullchain

--- haproxy/startup.sh
+++ haproxy/startup.sh
@@ -1,4 +1,8 @@
 echo ${EMAIL}
-/certbot/certbot-auto certonly --standalone --agree-tos -m ${EMAIL} -n -d ${EXTERNAL_URL}
-echo '@monthly root /certbot/certbot-auto certonly --quiet --standalone --renew-by-default -d ${EXTERNAL_URL} >> /var/log/certbot/certbot-auto-update.log' | tee --append /etc/crontab
+if ${GEN_CERT}; then
+    /certbot/certbot-auto certonly --standalone --agree-tos -m ${EMAIL} -n -d ${EXTERNAL_URL}
+else
+    echo "don't create ssl certificate"
+fi
+echo "@monthly root /certbot/certbot-auto certonly --quiet --standalone --renew-by-default -d ${EXTERNAL_URL} >> /var/log/certbot/certbot-auto-update.log" | tee --append /etc/crontab
 haproxy -f /usr/local/etc/haproxy/haproxy.cfg
