4f565d26251598aa63e34e756315fa50edf44307
Jonny <petmue@me.com>
2017-04-30 07:56:24 +0200

    reorder second half of haproxy.cfg

--- haproxy/startup.sh
+++ haproxy/startup.sh
@@ -23,23 +23,23 @@ if ${GEN_CERT}; then
     } >> /usr/local/etc/haproxy/haproxy.cfg
 else
     echo "don't create ssl certificate"
-    { \
-    echo "    acl has_web1 path_beg /web1"; \
-    echo "    acl has_web2 path_beg /web2"; \
-
-    echo "    use_backend web1 if has_web1"; \
-    echo "    use_backend web2 if has_web2"; \
-
-    echo "    default_backend web2"; \
-
-    echo "backend web1"; \
-    echo "    reqrep ^([^\ ]*\ /)web1[/]?(.*)     \1\2"; \
-    echo "    server web1 web1:80 check"; \
-
-    echo "backend web2"; \
-    echo "    reqrep ^([^\ ]*\ /)web2[/]?(.*)     \1\2"; \
-    echo "    server web2 web2:80 check"; \
-    } >> /usr/local/etc/haproxy/haproxy.cfg
 fi
+{ \
+echo "    acl has_web1 path_beg /web1"; \
+echo "    acl has_web2 path_beg /web2"; \
+
+echo "    use_backend web1 if has_web1"; \
+echo "    use_backend web2 if has_web2"; \
+
+echo "    default_backend web2"; \
+
+echo "backend web1"; \
+echo "    reqrep ^([^\ ]*\ /)web1[/]?(.*)     \1\2"; \
+echo "    server web1 web1:80 check"; \
+
+echo "backend web2"; \
+echo "    reqrep ^([^\ ]*\ /)web2[/]?(.*)     \1\2"; \
+echo "    server web2 web2:80 check"; \
+} >> /usr/local/etc/haproxy/haproxy.cfg
 echo "@monthly root /certbot/certbot-auto certonly --quiet --standalone --renew-by-default -d ${EXTERNAL_URL} >> /var/log/certbot/certbot-auto-update.log" | tee --append /etc/crontab
 haproxy -f /usr/local/etc/haproxy/haproxy.cfg
